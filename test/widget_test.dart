2d8291eaa013c572ddda75b2645a9d7347a88c37
azusa-f <azusa.fujinami@axia-works.com>
2022-03-17 14:01:49 +0900

    animation

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tutorial/main.dart';
+import 'package:tutorial/main_2.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
