7350376ab23909c99efe49563eefea8cc0c097bc
azusa-f <azusa.fujinami@axia-works.com>
2022-04-20 15:45:12 +0900

    MerukariItemをnullableに戻した

--- lib/merukari/mvvm/model/merukari_item.dart
+++ lib/merukari/mvvm/model/merukari_item.dart
@@ -7,10 +7,10 @@ part 'merukari_item.g.dart';
 @freezed
 class MerukariItem with _$MerukariItem {
   const factory MerukariItem({
-    @JsonKey(name: "imagePath") String imagePath,
-    @JsonKey(name: "articleName") String articleName,
-    @JsonKey(name: "articlePrice") String articlePrice,
-    @JsonKey(name: "articleInformation") String articleInformation,
+    @JsonKey(name: "imagePath") String? imagePath,
+    @JsonKey(name: "articleName") String? articleName,
+    @JsonKey(name: "articlePrice") String? articlePrice,
+    @JsonKey(name: "articleInformation") String? articleInformation,
   }) = _MerukariItem;
 
   factory MerukariItem.fromJson(Map<String, dynamic> json) =>
