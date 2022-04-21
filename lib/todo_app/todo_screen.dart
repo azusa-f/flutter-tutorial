e94f64de4d1251d47437daea9ec4de10e3b5c49c
azusa-f <azusa.fujinami@axia-works.com>
2022-04-21 15:50:32 +0900

    スライドしたら削除ボタン

--- lib/todo_app/todo_screen.dart
+++ lib/todo_app/todo_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:tutorial/todo_app/todo_db.dart';
 import 'package:tutorial/todo_app/todo_state_notifier.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoScreen extends ConsumerWidget {
   const TodoScreen({Key? key}) : super(key: key);
@@ -26,11 +27,21 @@ Widget _buildTodoList(_state, _notifier) {
           shrinkWrap: true,
           itemCount: _state.todoItems.length,
           itemBuilder: ((context, index) {
-            return SizedBox(
-              width: double.infinity,
-              child: Column(children: [
-                _buildToDo(_state.todoItems[index]),
+            return Slidable(
+              endActionPane:
+                  ActionPane(motion: const ScrollMotion(), children: [
+                SlidableAction(
+                  onPressed: (_) {},
+                  backgroundColor: Colors.red,
+                  icon: Icons.delete,
+                )
               ]),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(children: [
+                  _buildToDo(_state.todoItems[index]),
+                ]),
+              ),
             );
           }),
         ),
@@ -41,28 +52,35 @@ Widget _buildTodoList(_state, _notifier) {
 
 Widget _buildToDo(Todo todoItems) {
   DateFormat outputFormat = DateFormat('yyyy-MM-dd');
-  print(todoItems);
   return Padding(
     padding: const EdgeInsets.all(16.0),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('タイトル:${todoItems.title}'),
-        Text(todoItems.description, style: TextStyle(fontSize: 24)),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('作成日:${outputFormat.format(todoItems.addDate)}'),
-            const SizedBox(
-              width: 30,
-            ),
-            Text(
-              '期日:${outputFormat.format(todoItems.limitDate)}',
-              style: const TextStyle(color: Color.fromARGB(255, 211, 127, 17)),
-            ),
-          ],
+    child: Container(
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(),
         ),
-      ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('タイトル:${todoItems.title}'),
+          Text(todoItems.description, style: const TextStyle(fontSize: 24)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('作成日:${outputFormat.format(todoItems.addDate)}'),
+              const SizedBox(
+                width: 30,
+              ),
+              Text(
+                '期日:${outputFormat.format(todoItems.limitDate)}',
+                style:
+                    const TextStyle(color: Color.fromARGB(255, 211, 127, 17)),
+              ),
+            ],
+          ),
+        ],
+      ),
     ),
   );
 }
